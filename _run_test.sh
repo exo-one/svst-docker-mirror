9c64730473fbcbc33b17862162c1c5195d82b781
Max Kaye <m@xk.io>
2017-02-19 12:47:00 +1100

    Docker improvements - add dev-base-python - Update producer/dockerfile - Update _run_test.sh

--- _run_test.sh
+++ _run_test.sh
@@ -3,5 +3,6 @@
 set -e
 
 ./bin/regen-docker-test.sh
+docker-compose -f docker-compose.test.yml build --no-cache producer scraper header-download pallet-download pallet-verifier state-maker vote-explorer
 docker-compose -f docker-compose.test.yml -p ci up --build -d
 docker wait ci_sut_1
