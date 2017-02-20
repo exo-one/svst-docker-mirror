b154e38032567eebe3ce0df625271d392496db6d
Max Kaye <m@xk.io>
2017-02-20 11:58:40 +1100

    Add cache

--- _reset_tests.sh
+++ _reset_tests.sh
@@ -1,1 +1,6 @@
+docker pull exo1/dev-base-1
+docker pull exo1/dev-base-haskell
+docker pull exo1/dev-base-python
+docker pull exo1/dev-test-bitcoind
 docker-compose -f docker-compose.test.yml build --no-cache
+docker-compose -f docker-compose.test.yml build --no-cache bitcoind
