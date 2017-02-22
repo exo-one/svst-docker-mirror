ec96815c61fe5fdfd2c5a918f6984f2e4aeccef7
Max Kaye <m@xk.io>
2017-02-23 09:49:47 +1100

    Latest commit in massive refactor and reorganisation

--- _run_test.sh
+++ _run_test.sh
@@ -18,14 +18,21 @@ export VERIFY_SECRET_KEY=1234567890123456789012345678901234567890123456789012345
 export VERIFY_PUBKEY=c9df7bcba2238bedcc681e8b17bb21c1625d21d285b70c20cf53fdd473db9dfb
 
 docker-compose -f docker-compose.test.yml -p ci build --no-cache sut
-docker-compose -f docker-compose.test.yml -p ci up --build &  # don't use -d for tests so we get all logs
+
+containers=(header-download pallet-download producer scraper)
+for container in ${containers[@]}; do
+    ./_rebuild_ci_container.sh $container &
+done
+wait
+
+docker-compose -f docker-compose.test.yml -p ci up &  # don't use -d for tests so we get all logs
 
 until [[ ! -z $(get_sut_container_id) ]]; do
-    echo "### waiting for SUT ..."
+    echo "### waiting for SUT container ..."
     echo $(get_sut_container_id)
     sleep 10
 done
-echo "$$$ SUT UP... executing docker wait"
+echo "$$$ System Under Test (SUT) UP... executing docker wait"
 
 docker ps
 goodtest=$(docker wait $(get_sut_container_id))  # wait for SUT container to finish
