63f9efde5440c422e9c888d3689bb6aec9fcf18b
Max Kaye <m@xk.io>
2017-02-23 11:36:06 +1100

    Remove debug

--- _git_clone_all.sh
+++ _git_clone_all.sh
@@ -11,16 +11,10 @@ for repo in ${repos[@]}; do
   git clone "$gitroot/$repo"
   clonesuccess=$?
   if [ -n "$CI_BUILD_REF_NAME" ] && [ $clonesuccess -eq 0 ] ; then
-    echo "ls-ing in $(pwd)"
-    ls
     cd "$repo"
-    echo "ls-ing in $(pwd)"
-    ls
     echo "Checking out $CI_BUILD_REF_NAME based on env variable CI_BUILD_REF_NAME"
     echo "THIS IS NOT GUARENTEED TO BE THE SAME BRANCH!!! AND MAY FAIL IN FUTURE"
     git checkout "$CI_BUILD_REF_NAME"
     cd ..
-    echo "ls-ing in $(pwd)"
-    ls
   fi
 done
