45fa0f4379d7842e6611b08c72f601cda590a8c0
Max Kaye <m@xk.io>
2017-02-23 11:30:23 +1100

    Test success of git pull

--- _git_clone_all.sh
+++ _git_clone_all.sh
@@ -3,13 +3,14 @@
 source ./_git_repos.sh
 
 gitroot="https://gitlab.com/exo-one"
-if [[ -n "$SVST_DEV" ]]; then
+if [ -n "$SVST_DEV" ]; then
   gitroot="git@gitlab.com:exo-one"
 fi
 
 for repo in ${repos[@]}; do
   git clone "$gitroot/$repo"
-  if [[ -n "$CI_BUILD_REF_NAME" ]]; then
+  clonesuccess=$?
+  if [ -n "$CI_BUILD_REF_NAME" ] && [ $clonesuccess -eq 0 ] ; then
     echo "ls-ing in $(pwd)"
     ls
     cd "$repo"
